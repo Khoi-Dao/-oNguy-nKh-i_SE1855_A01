db7ecb7ceebee2cd73a485464bda6f63cabe921e
Dao Nguyen Khoi <khoi.dao2411@gmail.com>
2025-06-26 17:12:20 +0900

    WPF

---
+++ ĐàoNguyênKhôiWPF/AdminDashBoardWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BusinessObjects; // Assuming BusinessObjects namespace contains the Employees class
+using Services; // Assuming Services namespace contains the EmployeeService class
+
+namespace ĐàoNguyênKhôiWPF
+{
+    /// <summary>
+    /// Interaction logic for AdminDashBoardWindow.xaml
+    /// </summary>
+    public partial class AdminDashBoardWindow : Window
+    {
+
+        public AdminDashBoardWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
